8a324b336a4541cccb5433d71ef12125e6613b18
William Best <wmbest2@gmail.com>
2012-04-16 22:21:14 -0500

    Fixed JSR, added video callback

--- dcpu.js
+++ dcpu.js
@@ -28,33 +28,42 @@ operands = function(operand) {
         case 5:  return registers.Z;
         case 6:  return registers.I;
         case 7:  return registers.J;
-        case 8:  return memory[registers.A.value];
-        case 9:  return memory[registers.B.value];
-        case 10: return memory[registers.C.value];
-        case 11: return memory[registers.X.value];
-        case 12: return memory[registers.Y.value];
-        case 13: return memory[registers.Z.value];
-        case 14: return memory[registers.I.value];
-        case 15: return memory[registers.J.value];
-        case 16: return memory[registers.A.value + operands(31).value ];
-        case 17: return memory[registers.B.value + operands(31).value ];
-        case 18: return memory[registers.C.value + operands(31).value ];
-        case 19: return memory[registers.X.value + operands(31).value ];
-        case 20: return memory[registers.Y.value + operands(31).value ];
-        case 21: return memory[registers.Z.value + operands(31).value ];
-        case 22: return memory[registers.I.value + operands(31).value ];
-        case 23: return memory[registers.J.value + operands(31).value ];
-        case 24: return memory[registers.SP.value++];
-        case 25: return memory[registers.SP.value];
-        case 26: return memory[--registers.SP.value];
+        case 8:  return getmem(registers.A.value);
+        case 9:  return getmem(registers.B.value);
+        case 10: return getmem(registers.C.value);
+        case 11: return getmem(registers.X.value);
+        case 12: return getmem(registers.Y.value);
+        case 13: return getmem(registers.Z.value);
+        case 14: return getmem(registers.I.value);
+        case 15: return getmem(registers.J.value);
+        case 16: return getmem(registers.A.value + operands(31).value );
+        case 17: return getmem(registers.B.value + operands(31).value );
+        case 18: return getmem(registers.C.value + operands(31).value );
+        case 19: return getmem(registers.X.value + operands(31).value );
+        case 20: return getmem(registers.Y.value + operands(31).value );
+        case 21: return getmem(registers.Z.value + operands(31).value );
+        case 22: return getmem(registers.I.value + operands(31).value );
+        case 23: return getmem(registers.J.value + operands(31).value );
+        case 24: return getmem(registers.SP.value++);
+        case 25: return getmem(registers.SP.value);
+        case 26: return getmem(--registers.SP.value);
         case 27: return registers.SP; // SP
         case 28: return registers.PC; // PC
         case 29: return registers.O;  // O
-        case 30: return memory[operands(31).value];
-        case 31: return memory[registers.PC.value++];
+        case 30: return getmem(operands(31).value);
+        case 31: return getmem(registers.PC.value++);
         default: return {value: operand - 32};
     }
 };
+
+video = false;
+
+getmem = function(value) {
+    if (value >= 0x8000 && value < 0x8400) {
+        video = true;
+    }
+    return memory[value];
+}
 
 var getAAA = function(value) {
     return ((value >> 4) & 0x3f);
@@ -72,8 +81,9 @@ non_standard = {
     0: function(aaa) {},
     1: function(aaa) {
         //console.log("JSR");
+        val = operands(aaa).value
         operands(26).value = registers.PC.value;
-        registers.PC.value = operands(aaa).value;
+        registers.PC.value = val;
     },
 };
 
@@ -96,18 +106,22 @@ operations = {
         //console.log("SUB");
         opaaa = operands(aaa);
         opaaa.value -= operands(bbb).value; 
+        opaaa.value &= 0xffff;
     },
     4: function(aaa, bbb) { //MUL
         opaaa = operands(aaa);
         opaaa.value *= operands(bbb).value; 
+        opaaa.value &= 0xffff;
     },
     5: function(aaa, bbb) { //DIV
         opaaa = operands(aaa);
         opaaa.value /= operands(bbb).value; 
+        opaaa.value &= 0xffff;
     },
     6: function(aaa, bbb) { //MOD
         opaaa = operands(aaa);
         opaaa.value %= operands(bbb).value; 
+        opaaa.value &= 0xffff;
     },
     7: function(aaa, bbb) { //SHL
         opaaa = operands(aaa);
@@ -173,7 +187,7 @@ module.exports.init = function(instructions) {
     }
 }
 
-var step = function(step_cb, video_cb, input_cb) {
+module.exports.step = step = function(step_cb, video_cb, input_cb) {
     memval = memory[registers.PC.value].value;
     registers.PC.value++;
 
@@ -182,12 +196,21 @@ var step = function(step_cb, video_cb, input_cb) {
     if (step_cb) {
         step_cb();
     }
+
+    if (video) {
+        video_cb();
+        video = false;
+    }
 }
 
 module.exports.run = function(step_cb, video_cb, input_cb) {
     while (registers.PC.value < module.exports.program_len) {
         step(step_cb, video_cb, input_cb);
     }
+}
+
+module.exports.stop = function() {
+    registers.PC.value = module.exports.program_len;
 }
 
 module.exports.logRegisters = function() {
--- ncurses-display.js
+++ ncurses-display.js
@@ -13,8 +13,27 @@ module.exports.init = function(memory, start_value) {
 
     win = new nc.Window(h + 2, w + 2);
     win.frame();
+
+    pair = 0;
+    colors = nc.colorPair(pair, get_color(0), get_color(7));
+    win.attrset(colors);
     nc.showCursor = false;
 
+}
+
+get_color = function(bits) {
+    bits = bits % 8;
+
+    switch(bits) {
+        case 0: return nc.colors.BLACK;
+        case 1: return nc.colors.BLUE;
+        case 2: return nc.colors.GREEN;
+        case 3: return nc.colors.CYAN;
+        case 4: return nc.colors.RED;
+        case 5: return nc.colors.MAGENTA;
+        case 6: return nc.colors.YELLOW;
+        default: return nc.colors.WHITE;
+    }
 }
 
 module.exports.update = function() {
@@ -23,10 +42,38 @@ module.exports.update = function() {
         for (x = 0; x < w; ++x) {
         mem_loc = (x * w) + y;
         memval = mem[mem_loc + start].value;
-        c = memval & 0x00ff;
+        d = memval & 0x007f;
+        f = (memval & 0x0780) >> 7;
+        b = (memval & 0x7800) >> 11;
+        if (f == b && f == 0) {
+            b = 7;
+        }
+        blink = (memval & 0x8000) >> 15;
+        
+        if (blink == 1) {
+            win.attrset(nc.attrs.BLINK);
+        } else {
+            win.attrset(nc.attrs.NORMAL);
+        }
 
-        win.print(x + 1, y + 1, String.fromCharCode(c));
+        pair = 1;
+        colors = nc.colorPair(pair, get_color(f), get_color(b));
+        win.attrset(nc.colorPair(0));
+
+        win.print(x + 1, y + 1, String.fromCharCode(d));
         win.refresh();
         }
     }
+}
+
+module.exports.set = function(x, y, string) {
+
+    colors = nc.colorPair(0);
+    win.attrset(colors);
+
+    win.print(x, y, string);
+}
+
+module.exports.reset = function() {
+    if (win) win.close();
 }
